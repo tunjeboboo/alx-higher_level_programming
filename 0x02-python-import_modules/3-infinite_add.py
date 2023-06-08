1b54bed4d54343f35eafa82648ebfad5d5e02685
asofeyeje <drtunjiosungbade@gmail.com>
2023-06-08 07:50:53 -0700

    all

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-from sys import argv
-add = 0
-for s in argv[1:]:
-    add += int(s)
-print("{:d}".format(add))
+def add_arg(argv):
+    i = len(argv) - 1
+    if i == 0:
+        print("{:d}".format(i))
+        return
+    else:
+        j = 1
+        add = 0
+        while j <= i:
+            add += int(argv[i])
+            j += 1
+        print("{:d}".format(add))
+
+
+if __name__ == "__main__":
+    import sys
+    add_arg(sys.argv)
